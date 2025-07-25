9cc9027af45c406e304d4b15c8cce1db337146e2
Arkhand <musiald@gmail.com>
2025-07-25 08:23:00 -0300

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.bms.testtokenexchange.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
